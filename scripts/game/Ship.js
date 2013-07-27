b5143c57f9de167a633d5a1353bd3dcebe087c61
louis cabrera <louis.cabrera@uponamap.com>
2013-07-27 13:27:15 +0200

    ajout d'un tableau de joins

--- scripts/game/Ship.js
+++ scripts/game/Ship.js
@@ -2,14 +2,15 @@ define( [ "game/Box2D", "game/Collider" ], function( Box2D, Collider )
 {
 	var Ship = function(world) 
 	{
-		var modules = [];
+		this.modules = [];
+		this.joins = [];
 		for(var i = 0; i < 3; i++){
-			modules[i] = new Collider("square", [0.5,0.5], [10,0+i], world);
+			this.modules.push(new Collider("square", [0.5,0.5], [10,0+i], world));
 			if(i > 0 ) {
 				var jointDef = new Box2D.RevoluteJointDef();
-				jointDef.Initialize(modules[i-1].body.GetBody(), modules[i].body.GetBody(), modules[i].body.GetBody().GetWorldCenter());
+				jointDef.Initialize(modules[i-1].body.GetBody(), this.modules[i].body.GetBody(), this.modules[i].body.GetBody().GetWorldCenter());
 				jointDef.enableLimit = true;			
-				world.CreateJoint(jointDef);
+				this.joins.push(world.CreateJoint(jointDef));
 			}	
 		}
 		this.x;
