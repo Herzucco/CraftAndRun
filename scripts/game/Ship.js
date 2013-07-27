bbeb9feab023b55a7b7181a620ff83b93e564e4d
Adrien Carta <adrien.carta@laposte.net>
2013-07-27 06:20:46 +0200

    olo

--- scripts/game/Ship.js
+++ scripts/game/Ship.js
@@ -2,6 +2,9 @@ define( function() )
 {
 	var shipEntities = 
 	{
-		propulsion : { "components" : [ "renderShape" : { "color" : "hsl(125, 100%, 100%)", "shape" : "circle" } ] } 
+		propulsion : { "components" : [ {"renderShape" : { "color" : "hsl(125, 100%, 100%)", "shape" : "circle" }}, "shipParent", "module", {"propulsor" : 
+		{"speed" : "1"}}] };
+		 
+
 	}
 }
