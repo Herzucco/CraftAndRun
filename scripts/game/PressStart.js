b56f4f1b3d713a47becde2aae3d7c596ee330421
louis cabrera <louis.cabrera@uponamap.com>
2013-07-28 11:05:07 +0200

    splash screen

--- scripts/game/PressStart.js
+++ scripts/game/PressStart.js
@@ -1,9 +1,5 @@
 define( ["game/InputsManager"],function(InputsManager)
 {
-	var background = new Image ();
-    background.src = 'assets/menu/background.jpg';
-    var logo = new Image ();
-    logo.src = 'assets/menu/logo.png'
     var logoWidth = 814/3;
     var logoHeight = 734/3;
 	var PressStart = function( canvas, context )
@@ -13,20 +9,20 @@ define( ["game/InputsManager"],function(InputsManager)
 	
 	PressStart.prototype.update = function( deltaTime )
 	{
-				if(InputsManager.instance[13] == true)
-				{
-					InputsManager.instance[13] = false;
-					Game.initEditor();
-					Game.closePressStart();
-				}
+		if(InputsManager.instance[13] == true)
+		{
+			InputsManager.instance[13] = false;
+			Game.initEditor();
+			Game.closePressStart();
+		}
 	}
 	
 	PressStart.prototype.render = function( context, canvas)
 	{
 		context.fillStyle = "#FFAAFF";
 		context.fillRect(0,0,canvas.width, canvas.height);
-        context.drawImage(background,0,0,canvas.width,canvas.height);
-        context.drawImage(logo, canvas.width/2-logoWidth/2, 100, logoWidth, logoHeight);
+        context.drawImage(window.Images.bg,0,0,canvas.width,canvas.height);
+        context.drawImage(window.Images.logo, canvas.width/2-logoWidth/2, 100, logoWidth, logoHeight);
 	}
 
 
