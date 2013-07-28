040f407bb821646488ea0f1cb40bdaffe080d5ea
Gregoire Carabeufs <Gregoire.Carabeufs@gmail.com>
2013-07-28 10:23:16 +0200

    Menu suite
    intégration d'assets, press start.

--- scripts/game/PressStart.js
+++ scripts/game/PressStart.js
@@ -23,10 +23,14 @@ define( ["game/InputsManager"],function(InputsManager)
 	
 	PressStart.prototype.render = function( context, canvas)
 	{
-		context.fillStyle = "#FFAAFF";
+		context.fillStyle = "#000000";
 		context.fillRect(0,0,canvas.width, canvas.height);
         context.drawImage(background,0,0,canvas.width,canvas.height);
         context.drawImage(logo, canvas.width/2-logoWidth/2, 100, logoWidth, logoHeight);
+	    context.font = "Bold 47px MenuFont";
+	    var startText = "Press   Start";
+	    var dim = context.measureText(startText);
+	    context.fillText(startText, canvas.width / 2 - dim.width / 2, 900 / 2 - 25);
 	}
 
 
